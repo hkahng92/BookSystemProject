f0a67d1f9a9dbdd47db7d2604f9748f05f2cc388
surmurat <mrtsrnlr@gmail.com>
2019-07-30 10:52:16 -0400

    book service controller

--- Book-service/src/main/java/com/trilogyed/Bookservice/controller/BookServiceController.java
+++ Book-service/src/main/java/com/trilogyed/Bookservice/controller/BookServiceController.java
@@ -1,12 +1,58 @@
 package com.trilogyed.Bookservice.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.trilogyed.Bookservice.service.BookService;
+import com.trilogyed.Bookservice.viewmodel.BookViewModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
+@RequestMapping(value = "/books")
 public class BookServiceController {
 
+    @Autowired
+    BookService bookService;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BookViewModel createBook(@RequestBody BookViewModel book){
+        return bookService.newBook(book);
+    }
 
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public BookViewModel getBook(@PathVariable("id") int id){
+        BookViewModel book = bookService.fetchBook(id);
+        if(book == null)
+            throw new IllegalArgumentException("Book could not be retrieved for id " + id);
+        return book;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<BookViewModel> getAllBooks(){
+        return bookService.fetchAllBooks();
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateBook(@PathVariable("id") int bookId, @RequestBody @Valid BookViewModel bookViewModel){
+        if(bookViewModel.getBookId()==0)
+            bookViewModel.setBookId(bookId);
+        if ((bookId != bookViewModel.getBookId())){
+            throw new IllegalArgumentException("Book ID on path must match the ID in the Book object");
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBook(@PathVariable("id") int bookId){
+        bookService.deleteBook(bookId);
+    }
 
     //CRUD and get all controller
 }
